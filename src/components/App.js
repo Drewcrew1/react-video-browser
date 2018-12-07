0ef96e0210a282592e564332c2bcd309cd834ba0
Drew <drewharper29@gmail.com>
2018-12-07 13:17:51 -0500

    added video item component and structured videolist component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
+    state = { videos: []};
+
+    onTermSubmit = (term) => {
+        youtube.get('/search', {
+            params: {
+                q: term
+            }
+        }).then((response) => {
+            this.setState({videos: response.data.items});
+        });
+    };
+
     render() {
         return (
             <div className="ui contatiner">
-                <SearchBar/>
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
