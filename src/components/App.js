ef944dcd97ecfb5099f6497e62ac53377aea4746
Drew <drewharper29@gmail.com>
2018-12-08 10:41:56 -0500

    added styling for video list and video items along with embeded video rendering

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-    state = { videos: []};
+    state = { videos: [], selectedVideo: null};
 
     onTermSubmit = (term) => {
         youtube.get('/search', {
@@ -16,11 +17,24 @@ class App extends React.Component {
         });
     };
 
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo : video});
+    };
+
     render() {
         return (
             <div className="ui contatiner">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <VideoList videos={this.state.videos}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                <VideoDetail video={this.state.selectedVideo}/>
+                        </div>
+                        <div className="five wide column">
+                <VideoList  onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                        </div>
+                        </div>
+                    </div>
             </div>
         );
     }
